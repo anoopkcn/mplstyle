c9caf515622c2cb1b6ee7fe38a39ecc1013bf700
Anoop Chandran <anoopkcn@gmail.com>
2022-04-11 19:18:40 +0200

    Individual context for subplots

---
+++ examples/subplots.py
@@ -0,0 +1,24 @@
+import matplotlib as mpl # To use context manager of matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+
+
+x = np.linspace(-np.pi, np.pi, 100)
+
+fig = plt.figure(figsize=(10, 4))
+
+with mpl.rc_context( {"axes.grid": True, "xtick.minor.visible": True, "ytick.minor.visible": True} ):
+    ax1 = fig.add_subplot(1, 2, 1)
+    ax1.plot(x, np.sin(x))
+
+with mpl.rc_context( {"text.usetex":False} ):
+    ax2 = fig.add_subplot(1, 2, 2)
+    ax2.plot(x, np.cos(x),color='tab:green')
+
+with mpl.rc_context( {"axes.grid": True, "xtick.minor.visible": True, "ytick.minor.visible": True} ):
+    panel = ax2.inset_axes([0.35,0.2,0.3,0.3])
+    panel.plot(x, np.sin(x), color="tab:orange")
+
+plt.show()
