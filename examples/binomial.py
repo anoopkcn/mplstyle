d91ad4ace78d91d5ee2eaaee37b11e33495e43b9
Anoop K. Chandran <anoopkcn@gmail.com>
2024-10-16 20:00:14 +0200

    update examples

--- examples/binomial.py
+++ examples/binomial.py
@@ -1,16 +1,16 @@
-from scipy.stats import binom
-import matplotlib as mpl # To use context manager of matplotlib
+import math
+
+import matplotlib as mpl  # To use context manager of matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-import math
 
 from mplstyle import styles
 
-st=styles.Style('academic')
+st = styles.Style("academic")
 
 
 def res(n, p, x):
-    return math.comb(n, x) * p ** x * (1 - p) ** (n - x)
+    return math.comb(n, x) * p**x * (1 - p) ** (n - x)
 
 
 n, p = 30 * 75, 1 / 900
@@ -23,20 +23,20 @@ color = ["tab:blue"] * len(x)
 color[0] = "tab:orange"
 
 
-with mpl.rc_context({"axes.grid": True}):
-    '''
+with mpl.rc_context({"axes.grid": True, "axes.axisbelow": True}):
+    """
     The default behavior of academic style is not to draw a grid on the axes.
-    '''
+    """
     plt.bar(x, p_x, color=color)
     plt.xticks(x)
-    plt.ylabel("$\mathrm{P(X)}$")
+    plt.ylabel("$\\mathrm{P(X)}$")
     plt.xlabel("$X$")
     plt.title("Probability of triggering the threshold")
     plt.annotate(
         "$P(X=0) = 0.082$", xy=(x.max() / 2.0, p_x.max() / 2), ha="left", va="center"
     )
     plt.annotate(
-        "$P(X\geq1) = 0.918$",
+        "$P(X\\geq1) = 0.918$",
         xy=(x.max() / 2.0, p_x.max() / 2 - 0.03),
         ha="left",
         va="center",
@@ -47,4 +47,4 @@ with mpl.rc_context({"axes.grid": True}):
         ha="left",
         va="center",
     )
-plt.show() # This could be inside of outside the context 
+plt.show()  # This could be inside of outside the context
--- examples/subplots.py
+++ examples/subplots.py
@@ -1,25 +1,29 @@
-import matplotlib as mpl 
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 
 from mplstyle import styles
 
-st=styles.Style('academic')
+st = styles.Style("academic")
 
 x = np.linspace(-np.pi, np.pi, 100)
 
 fig = plt.figure(figsize=(10, 4))
 
-with mpl.rc_context( {"axes.grid": True, "xtick.minor.visible": True, "ytick.minor.visible": True} ):
+with mpl.rc_context(
+    {"axes.grid": True, "xtick.minor.visible": True, "ytick.minor.visible": True}
+):
     ax1 = fig.add_subplot(1, 2, 1)
     ax1.plot(x, np.sin(x))
 
-with mpl.rc_context( {"text.usetex":False} ):
+with mpl.rc_context({"text.usetex": False}):
     ax2 = fig.add_subplot(1, 2, 2)
-    ax2.plot(x, np.cos(x),color='tab:green')
+    ax2.plot(x, np.cos(x), color="tab:green")
 
-with mpl.rc_context( {"axes.grid": True, "xtick.minor.visible": True, "ytick.minor.visible": True} ):
-    panel = ax2.inset_axes([0.35,0.2,0.3,0.3])
+with mpl.rc_context(
+    {"axes.grid": True, "xtick.minor.visible": True, "ytick.minor.visible": True}
+):
+    panel = ax2.inset_axes((0.35, 0.2, 0.3, 0.3))
     panel.plot(x, np.sin(x), color="tab:orange")
 
 plt.show()
