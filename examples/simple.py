d680c46566f2d41095b8dd7dba9884ac746a21c3
Anoop K. Chandran <anoopkcn@gmail.com>
2024-10-17 19:55:14 +0200

    change figure size

--- examples/simple.py
+++ examples/simple.py
@@ -4,7 +4,7 @@ import numpy as np
 from plotreset import Styles
 
 style = Styles("reset")
-plt.figure()
+plt.figure(figsize=(6, 4))
 x = np.linspace(-5, 5, 200)
 y = 1 / (np.sqrt(2 * np.pi)) * np.exp(-(x**2) / 2)
 y2 = 1 / (np.sqrt(2 * np.pi * 2)) * np.exp(-(x**2) / (2 * 0.5))
